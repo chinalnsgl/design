d83c2d7cf8b5260ffcfde9071db264d6a993be8f
China.sgl <251380500@qq.com>
2018-09-05 10:40:57 +0800

    commit

--- src/main/java/com/zw/design/DesignApplication.java
+++ src/main/java/com/zw/design/DesignApplication.java
@@ -3,12 +3,19 @@ package com.zw.design;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.zw.design.mapper")
-public class DesignApplication {
+public class DesignApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DesignApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DesignApplication.class);
     }
 }
