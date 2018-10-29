abc28f43d75deaaf7f8694bb3c1d589b6ad387cf
China.sgl <251380500@qq.com>
2018-10-29 16:33:09 +0800

    修改文字说明

--- src/main/java/com/zw/design/utils/Const.java
+++ src/main/java/com/zw/design/utils/Const.java
@@ -6,7 +6,7 @@ public interface Const {
     String[] HYPRE_STEP_NAME = {"液压设计","提委托书","报采购计划","下图纸"};
     String[] ELECTRIC_STEP_NAME = {"电气设计","确定最终条件","报采购计划","下图纸"};
     String[] SOFTWARE_STEP_NAME = {"软件设计","编码及测试","报采购计划","试运行"};
-    String[] PRODUCE_TASK_STEP_NAME = {"下任务单","签订协议","签合同","原材料外协计划","工艺编制","外购计划","铆焊制作","机加外协装配","现场安装","调试运行","验收","图纸存档"};
+    String[] PRODUCE_TASK_STEP_NAME = {"任务单","技术协议","合同","原材料外协计划","工艺编制","外购计划","铆焊制作","机加外协装配","现场安装","调试运行","验收","图纸存档"};
 
     String[] DEPARTMENT_NAME = {"","机械设计","液压设计","电气设计","软件设计"};
     String[] DEPARTMENT_NUM = {"","一科","二科","三科","四科","五科","六科","七科","八科"};
